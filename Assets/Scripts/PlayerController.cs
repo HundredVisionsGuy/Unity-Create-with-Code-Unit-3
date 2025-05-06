d3be8312def0d469114d36c7b950e9f2b12d072b
hundredvisionsguy <winikkc@hsd.k12.or.us>
2025-05-06 14:17:01 -0700

    adding double-jump:

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,34 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool canJump = true;
+    int numJumps = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up * 1000);
-    }
+        Physics.gravity *= gravityModifier;
+    } 
 
     // Update is called once per frame
     void Update()
     {
-        
+        canJump = numJumps < 2;
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            numJumps++;
+        }
+    }
+
+    // Once we land, we can jump
+    private void OnCollisionEnter(Collision collision)
+    {
+        numJumps = 0;
     }
 }
