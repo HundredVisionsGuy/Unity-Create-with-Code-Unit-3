fa19faf93db3712f3610ae042b123fcaf150c46e
hundredvisionsguy <winikkc@hsd.k12.or.us>
2025-05-07 17:08:02 -0700

    adding spawning obstacles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,31 +6,36 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
-    public bool canJump = true;
-    int numJumps = 0;
+    public bool gameOver = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
+        playerRb = GetComponent<Rigidbody>(); 
         Physics.gravity *= gravityModifier;
     } 
 
     // Update is called once per frame
     void Update()
     {
-        canJump = numJumps < 2;
-        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 
-            numJumps++;
+            isOnGround = false;
         }
     }
 
     // Once we land, we can jump
     private void OnCollisionEnter(Collision collision)
     {
-        numJumps = 0;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over, man!");
+        }
     }
 }
